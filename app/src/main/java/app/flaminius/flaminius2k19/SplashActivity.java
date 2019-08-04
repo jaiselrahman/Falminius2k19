f10ac5c3b9eea53486345649e427a119cd6adae1
jaiselrahman <jaiselrahman@gmail.com>
2019-08-04 12:40:22 +0530

    Add splash screen

---
+++ app/src/main/java/app/flaminius/flaminius2k19/SplashActivity.java
@@ -0,0 +1,16 @@
+package app.flaminius.flaminius2k19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
